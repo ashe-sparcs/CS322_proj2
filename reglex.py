c09f3ef101eceee47ea0f79e2faf78430146adb8
ashe-sparcs <ashe@sparcs.org>
2016-11-22 22:43:04 +0900

    making ast complete.

--- reglex.py
+++ reglex.py
@@ -5,32 +5,34 @@
 # symbols and +,*
 # ------------------------------------------------------------
 import ply.lex as lex
+import ply.yacc as yacc
 
 # List of token names.   This is always required
 tokens = (
    'SYMBOL',
    'PLUS',
-   'TIMES',
+   'ASTERISK',
    'LPAREN',
    'RPAREN',
+   'CONCAT',
 )
 
 # Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_TIMES   = r'\*'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+t_PLUS       = r'\+'
+t_ASTERISK   = r'\*'
+t_LPAREN     = r'\('
+t_RPAREN     = r'\)'
+# t_CONCAT     = r'^$'
+t_CONCAT     = r'\.'
+
 
 # A regular expression rule with some action code
-
-
 def t_SYMBOL(t):
-    r'[0-9a-zA-Z]'
+    r'[0-9a-zA-Z] | \(\)'
     return t
 
+
 # Define a rule so we can track line numbers
-
-
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
@@ -38,9 +40,8 @@ def t_newline(t):
 # A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t'
 
+
 # Error handling rule
-
-
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
@@ -59,3 +60,59 @@ lexer.input(data)
 print('using iteration protocol')
 for tok in lexer:
     print(tok.value, tok.type)
+
+
+# Yacc example
+# Get the token map from the lexer.  This is required.
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+
+def p_expression_plus(p):
+    '''expression : expression PLUS expression'''
+
+    p[0] = (p[2], p[1], p[3])
+
+
+def p_expression_asterisk(p):
+    '''expression : expression ASTERISK'''
+    p[0] = (p[2], p[1])
+
+
+def p_expression_group(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
+def p_expression_symbol(p):
+    'expression : SYMBOL'
+    p[0] = ('symbol', p[1])
+
+
+def p_expression_concat(p):
+    'expression : expression expression'
+    p[0] = ('.', p[1], p[2])
+
+
+'''
+def make_enfa(s):
+    if s[0] == 'symbol':
+'''
+
+
+# Build the parser
+parser = yacc.yacc()
+
+while True:
+    try:
+        s = input('regex > ')
+        lexer.input(s)
+        s = ''.join([x.value for x in lexer])
+        print(s)
+    except EOFError:
+        break
+    if not s:
+        continue
+    result = parser.parse(s)
+    print(result)
